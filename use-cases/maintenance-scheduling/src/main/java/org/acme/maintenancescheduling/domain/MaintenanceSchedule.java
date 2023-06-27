2cff9d7a348c9e26d79eec7dc50a33c2211cfcd3
viniciusedsonn <89812593+viniciusedsonn@users.noreply.github.com>
2023-06-27 01:22:52 -0300

    Initialize 8.15.x

---
+++ use-cases/maintenance-scheduling/src/main/java/org/acme/maintenancescheduling/domain/MaintenanceSchedule.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.acme.maintenancescheduling.domain;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.optaplanner.core.api.domain.solution.PlanningEntityCollectionProperty;
+import org.optaplanner.core.api.domain.solution.PlanningScore;
+import org.optaplanner.core.api.domain.solution.PlanningSolution;
+import org.optaplanner.core.api.domain.solution.ProblemFactCollectionProperty;
+import org.optaplanner.core.api.domain.solution.ProblemFactProperty;
+import org.optaplanner.core.api.domain.valuerange.ValueRangeProvider;
+import org.optaplanner.core.api.score.buildin.hardsoftlong.HardSoftLongScore;
+import org.optaplanner.core.api.solver.SolverStatus;
+
+@PlanningSolution
+public class MaintenanceSchedule {
+
+    @ProblemFactProperty
+    private WorkCalendar workCalendar;
+    @ProblemFactCollectionProperty
+    @ValueRangeProvider(id = "crewRange")
+    private List<Crew> crewList;
+    @PlanningEntityCollectionProperty
+    private List<Job> jobList;
+
+    @PlanningScore
+    private HardSoftLongScore score;
+
+    // Ignored by OptaPlanner, used by the UI to display solve or stop solving button
+    private SolverStatus solverStatus;
+
+    // No-arg constructor required for OptaPlanner
+    public MaintenanceSchedule() {
+    }
+
+    public MaintenanceSchedule(WorkCalendar workCalendar,
+            List<Crew> crewList, List<Job> jobList) {
+        this.workCalendar = workCalendar;
+        this.crewList = crewList;
+        this.jobList = jobList;
+    }
+
+    @ValueRangeProvider(id = "startDateRange")
+    public List<LocalDate> createStartDateList() {
+        return workCalendar.getFromDate().datesUntil(workCalendar.getToDate())
+                // Skip weekends. Does not work for holidays.
+                // Keep in sync with EndDateUpdatingVariableListener.updateEndDate().
+                // To skip holidays too, cache all working days in WorkCalendar.
+                .filter(date -> date.getDayOfWeek() != DayOfWeek.SATURDAY
+                        && date.getDayOfWeek() != DayOfWeek.SUNDAY)
+                .collect(Collectors.toList());
+    }
+
+    // ************************************************************************
+    // Getters and setters
+    // ************************************************************************
+
+    public WorkCalendar getWorkCalendar() {
+        return workCalendar;
+    }
+
+    public List<Crew> getCrewList() {
+        return crewList;
+    }
+
+    public List<Job> getJobList() {
+        return jobList;
+    }
+
+    public HardSoftLongScore getScore() {
+        return score;
+    }
+
+    public void setScore(HardSoftLongScore score) {
+        this.score = score;
+    }
+
+    public SolverStatus getSolverStatus() {
+        return solverStatus;
+    }
+
+    public void setSolverStatus(SolverStatus solverStatus) {
+        this.solverStatus = solverStatus;
+    }
+}
---
+++ use-cases/vehicle-routing/src/test/java/org/acme/vehiclerouting/solver/SolverManagerTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.acme.vehiclerouting.solver;
+
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.acme.vehiclerouting.bootstrap.DemoDataBuilder;
+import org.acme.vehiclerouting.domain.Location;
+import org.acme.vehiclerouting.domain.VehicleRoutingSolution;
+import org.junit.jupiter.api.Test;
+import org.optaplanner.core.api.solver.SolverManager;
+
+@QuarkusTest
+class SolverManagerTest {
+
+    @Inject
+    SolverManager<VehicleRoutingSolution, Long> solverManager;
+
+    @Test
+    void solve() throws ExecutionException, InterruptedException {
+        VehicleRoutingSolution problem = DemoDataBuilder.builder().setMinDemand(1).setMaxDemand(2)
+                .setVehicleCapacity(25).setCustomerCount(75).setVehicleCount(6).setDepotCount(2)
+                .setSouthWestCorner(new Location(0L, 43.751466, 11.177210))
+                .setNorthEastCorner(new Location(0L, 43.809291, 11.290195)).build();
+        solverManager.solve(0L, id -> problem, SolverManagerTest::printSolution).getFinalBestSolution();
+    }
+
+    static void printSolution(VehicleRoutingSolution solution) {
+        solution.getVehicleList().forEach(vehicle -> System.out.printf("%s: %s%n", vehicle,
+                vehicle.getRoute().stream().map(Location::getId).collect(Collectors.toList())));
+    }
+}
