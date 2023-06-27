b8a2fc92453a2b8b869535588f406e63fbdd3fc1
viniciusedsonn <89812593+viniciusedsonn@users.noreply.github.com>
2023-06-27 01:23:00 -0300

    Initialize 8.23.x

---
+++ use-cases/maintenance-scheduling/src/main/java/org/acme/maintenancescheduling/domain/WorkCalendar.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.acme.maintenancescheduling.domain;
+
+import java.time.LocalDate;
+import java.util.List;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class WorkCalendar {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private LocalDate fromDate; // Inclusive
+    private LocalDate toDate; // Exclusive
+
+    // No-arg constructor required for Hibernate
+    public WorkCalendar() {
+    }
+
+    public WorkCalendar(LocalDate fromDate, LocalDate toDate) {
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+    }
+
+    @Override
+    public String toString() {
+        return fromDate + " - " + toDate;
+    }
+
+    // ************************************************************************
+    // Getters and setters
+    // ************************************************************************
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDate getFromDate() {
+        return fromDate;
+    }
+
+    public LocalDate getToDate() {
+        return toDate;
+    }
+
+}
