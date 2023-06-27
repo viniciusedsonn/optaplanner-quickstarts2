e075acbb1b5dbf039b5ef3900dc5a990052487d8
viniciusedsonn <89812593+viniciusedsonn@users.noreply.github.com>
2023-06-27 01:22:59 -0300

    Initialize 8.22.x

---
+++ use-cases/vehicle-routing/src/main/java/org/acme/vehiclerouting/domain/Customer.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.acme.vehiclerouting.domain;
+
+public class Customer {
+
+    private long id;
+    private Location location;
+    private int demand;
+
+    public Customer() {
+    }
+
+    public Customer(long id, Location location, int demand) {
+        this.id = id;
+        this.location = location;
+        this.demand = demand;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public int getDemand() {
+        return demand;
+    }
+
+    public void setDemand(int demand) {
+        this.demand = demand;
+    }
+
+    // ************************************************************************
+    // Complex methods
+    // ************************************************************************
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                '}';
+    }
+}
